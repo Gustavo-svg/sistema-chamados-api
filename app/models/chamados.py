b06943f9e49b9c1f828dadb35ea5729a03a2a1ce
Gustavo Bianchi <SEU_EMAIL_DO_GITHUB@gmail.com>
2026-01-29 13:04:47 -0300

    Criado o modelo da tabela de chamados e instanciado a coluna id dos usuairos junto com as do chamados

--- app/models/chamados.py
+++ app/models/chamados.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer , String, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from datetime import datetime
 from app.database import Base
 
 class Chamado(Base):
     __tablename__ = "chamados"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     titulo = Column(String(255), nullable=False)
     descricao = Column(Text, nullable=False)
-    usuario_id = Column(Integer, ForeignKey("usuario.id"))
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
     criado_em = Column(DateTime, default=datetime.utcnow)
